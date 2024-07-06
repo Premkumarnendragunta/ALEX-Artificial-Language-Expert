bc90fc1bf32fc7b49d6f4d638cf1a7b8a51e5a3e
Prem <154546838+Premkumarnendragunta@users.noreply.github.com>
2024-07-06 05:51:35 -0700

    Update views.py

--- views.py
+++ views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from google.generativeai import GenerativeModel, configure
 
-# Create your views here.
+configure(api_key="your api key")  
+model = GenerativeModel("gemini-1.5-pro")
+
+def index(request):
+    return render(request, "index.html")
+
+def ai_response(request):
+    if request.method == 'POST':
+        user_input = request.POST.get("x")
+        print("User Input:", user_input)  
+        response = model.generate_content(user_input)
+        print("Gemini Response:", response.text)
+        return render(request, "index.html", {"response": response.text})
+    else:
+        return render(request, "index.html")
